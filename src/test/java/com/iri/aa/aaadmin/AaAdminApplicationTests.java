7008e8001d63c43043cfb57e30c5020eb0c98e14
Konstantinos Papadopoulos <Konstantinos.Papadopoulos@IRIworldwide.com>
2019-11-15 18:32:50 +0200

    Initial approach of a Spring Boot Admin Server.

---
+++ src/test/java/com/iri/aa/aaadmin/AaAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iri.aa.aaadmin;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AaAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
